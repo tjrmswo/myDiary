ee2f1c60c93791ebb379cf9e56f26614b40ecd32
Seo_KeunJae <tjrmswo506@naver.com>
2023-10-16 16:34:55 +0900

    feat: 로그인 회원가입 페이지 기능만 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+//recoil
+import { RecoilRoot } from 'recoil';
+
 //pages
 import Login from './pages/Login';
 import MainPage from './pages/MainPage';
+import Signup from './pages/Signup';
 const App = () => {
 
   return (
-
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/MainPage' element={<MainPage />} />
-    </Routes>
-
-
+    <RecoilRoot>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/MainPage' element={<MainPage />} />
+        <Route path='/Signup' element={<Signup />} />
+      </Routes>
+    </RecoilRoot>
   );
 }
 
