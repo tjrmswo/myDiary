7d070f1554bd612503d69a5f181f69389feab485
Seo_KeunJae <tjrmswo506@naver.com>
2024-01-05 00:19:46 +0900

    feat: 전체적인 디자인 변경

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
+
+// libraries
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { currentId } from "../util/atom";
 
+//img
+import LogoImg from "../assets/Logo.png";
+import UserImg from "../assets/user_custom.png";
+import LoginBlackImg from "../assets/login_black.png";
+import CalendarImg from "../assets/calendar.png";
+import LocationImg from "../assets/location.png";
+import PlusImg from "../assets/plus.png";
+
 const Login = () => {
   const navigation = useNavigate();
-
-  const [background, setBackground] = useState(false);
   const [userData, setUserData] = useState({
-    id: '',
-    password: '',
+    id: "",
+    password: "",
   });
   const [currentUser, setCurrentUser] = useRecoilState(currentId);
 
@@ -21,47 +29,49 @@ const Login = () => {
     const { name, value } = e.target;
     setUserData({
       ...userData,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
   const logIn = () => {
     if (localStorage.getItem(id) && searchPassword.password === password) {
       localStorage.setItem(`cnt${id}`, id);
       setCurrentUser(id);
-      navigation('/MainPage')
+      navigation("/MainPage");
     } else {
-      alert('아이디 또는 비밀번호를 확인해주세요')
+      alert("아이디 또는 비밀번호를 확인해주세요");
     }
-  }
+  };
 
   const goAssign = () => {
-    navigation('/Signup')
-  }
+    navigation("/Signup");
+  };
 
   useEffect(() => {
     console.log("id: ", id);
     console.log("currentUser: ", currentUser);
-  }, [id, currentUser])
+  }, [id, currentUser]);
 
   return (
-    <Wrapper isdark={background.toString()}>
-      <Loginbox>
-        <div className="headerContainer">
-          <Diarytitle>나만의 일기장 만들기</Diarytitle><ChangeTheme onClick={() => setBackground(!background)}>darkmode</ChangeTheme>
-        </div>
-        <div className="bodyConatiner">
-          <div className="Id">ID</div>
-          <Inputcontainer name='id' value={id} type="ID" required onChange={inputData} />
-          <div className="Password">Password</div>
-          <Inputcontainer name="password" value={password} type="password" required onChange={inputData} />
-        </div>
-        <LoginButton onClick={logIn}>로그인</LoginButton>
-        <AssignButton onClick={goAssign}>회원 가입</AssignButton>
-      </Loginbox>
+    <Wrapper>
+      <div className="header">
+        <LogoImage src={LogoImg} />
+        <div className="title">MyDiary</div>
+        <UserImage src={UserImg} />
+      </div>
+      <div className="drawer">
+        <LocationImage src={LocationImg} />
+        <CalendarImage src={CalendarImg} />
+        <PlusImage src={PlusImg} />
+      </div>
+      <div className="row">
+        <LoginImage src={LoginBlackImg} />
+        <div className="signincomment">Sing in to write your diary</div>
+        <SigninButton>Sign in</SigninButton>
+      </div>
     </Wrapper>
   );
-}
+};
 
 export default Login;
 
@@ -71,57 +81,84 @@ const Wrapper = styled.div`
   height: 100vh;
   align-items: center;
   justify-content: center;
-  ${(props) => props.isdark === "true" ? 'background-color: #757674' : 'background-color: white'}
+  background-color: #f6fdff;
+
+  .drawer {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    left: 0;
+    width: 5vw;
+    height: 100vh;
+    background-color: #232323;
+  }
+  .header {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 0;
+    width: 100vw;
+    height: 10vh;
+    background-color: #e9f6fb;
+  }
+  .title {
+    font-size: 1.2rem;
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    color: #23b1dc;
+    font-weight: bold;
+  }
+  .row {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  .signincomment {
+    margin-top: 0.5rem;
+    color: gray;
+  }
 `;
 
-const Loginbox = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-direction: column;
-
-  box-shadow: 2px 2px 12px gray;
-  background-color: #F2F2F2;
-  width: 38vw;
-  height: 60vh;
-  border-radius: 1em;
-  
-  .headerContainer {
-    margin-bottom: 1rem;
-  }
-  .Id {
-    font-size: 1rem;
-    margin-bottom: 0.5rem;
-    font-weight: 550;
-  }
-  .bodyConatiner { 
-    margin-bottom: 5em;
-  }
-  .Password {
-    font-size: 1rem;
-    margin-bottom: 0.5rem;
-    font-weight: 550;
-  }
-`
-const Diarytitle = styled.span`
-  font-size: 2em;
-  font-weight: bold;
-`
-const ChangeTheme = styled.button`
-  font-weight: bold;
+const LogoImage = styled.img`
+  width: 4vw;
+  height: 8vh;
 `;
 
-const Inputcontainer = styled.input`
-  width: 30vw;
-  height: 3vh;
+const LoginImage = styled.img`
+  width: 5vw;
+  margin-top: 2rem;
 `;
 
-const LoginButton = styled.button`
-  width: 20vw;
-  height: 5vh;
-`
+const UserImage = styled.img`
+  position: absolute;
+  right: 3rem;
+  width: 2.5vw;
+`;
 
-const AssignButton = styled.button`
-  width: 20vw;
-  height: 5vh;
-`
+const PlusImage = styled.img`
+  width: 2vw;
+  margin-top: 2rem;
+`;
+
+const CalendarImage = styled.img`
+  width: 2vw;
+  margin-top: 2rem;
+`;
+
+const LocationImage = styled.img`
+  width: 2vw;
+  margin-top: 2rem;
+`;
+
+const SigninButton = styled.button`
+  width: 6vw;
+  height: 4.5vh;
+  margin-top: 1rem;
+  border: none;
+  border-radius: 1rem;
+  background-color: #23b1dc;
+  color: white;
+`;
